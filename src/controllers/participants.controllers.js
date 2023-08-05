33dea4d60a6fb8b6bd575a4f06f4ef19fe5b6451
Diego Tuesta <diego.productividad@gmail.com>
2023-08-04 23:00:09 -0500

    se add validation participants y conversations 4

--- src/controllers/participants.controllers.js
+++ src/controllers/participants.controllers.js
@@ -4,6 +4,8 @@ const addParticipants = async (req, res, next) => {
     try {
       const { userId, conversationId } = req.body;
 
+      if(userId.length = 0  ) return res.status(400).json(message("Add Participants"))  ;
+
       const participantsArray = userId.map((participant) => ({
         userId: participant,
         conversationId
--- src/routes/messages.routes.js
+++ src/routes/messages.routes.js
@@ -3,8 +3,11 @@ const { createMessage, getMessagesUser,deleteMessage  } = require("../controller
 const authenticate = require("../middlewares/auth.middleware");
 const router = Router();
 
+//crear mensaje
 router.post("/messages", authenticate, createMessage)
+//obtener todos los mensajes de un usuario
 router.get("/messages/:id", authenticate, getMessagesUser)
+//eliminar mensaje
 router.delete("/messages/:id", authenticate, deleteMessage)
 
 
